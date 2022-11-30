08d67a3bec241bf54baa4f1ea2ed31e5a0d50765
Dominguez <migueldominguezroman01@gmail.com>
2022-11-30 11:10:00 +0100

    30/11/2022 - Barra Lateral y Login

---
+++ JS/login.js
@@ -0,0 +1,15 @@
+onload = ()=>{
+    changeWidthInput(document.getElementById("userEmail"))
+}
+
+const changeWidthInput = (id)=>{
+    id.style.width = "120px"
+}
+const changeHeigthInput = (id)=>{
+    id.style.height = "20px"
+}
+
+
+const changeDiplay= (id, display) =>{
+    id.style.display = display
+}
