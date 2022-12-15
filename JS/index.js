5d41ea545c4924167a2debdf7a4b82a66e0e5f85
Dominguez <migueldominguezroman01@gmail.com>
2022-12-15 12:07:35 +0100

    15/12/2022 - Prueba pull request

--- JS/index.js
+++ JS/index.js
@@ -1,4 +1,15 @@
 onload = () =>{
+
     document.getElementById("headerIndex").style.width = innerWidth
+    agrandar()
+    subir()
+}
+
+
+function subir(){
+    document.getElementById("textHotel").style.marginTop = "1em"
+} 
+function agrandar(){
+    document.getElementById("textHotel").style.fontSize = "75px"
 }
 //Prueba Fer
