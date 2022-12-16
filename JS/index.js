9db6da23952ad8623d6aa8e83ef1f58e06a82675
Ivan Cano <ivan.canyus@educa.jcyl.es>
2022-12-16 11:58:43 +0100

    16/12/2022 Slider arreglado sin tiempo Cano

--- JS/index.js
+++ JS/index.js
@@ -10,11 +10,10 @@ let slideIndex = 0;
 let botonNext = document.getElementById("next");
 let botonPrev = document.getElementById("prev");
 let slides = document.getElementsByClassName("mySlides");
-let tiempo = 10000;
 showSlides();
 
 function botonDerecha() {
-    slideIndex++;
+    slideIndex+2;
   showSlides();
 }
 
@@ -33,8 +32,6 @@ function showSlides() {
     slideIndex = 1;
   }
   slides[slideIndex - 1].style.display = "block";
-
-  setTimeout(showSlides,tiempo);
 
   botonNext.addEventListener("click", botonDerecha);
   botonPrev.addEventListener("click", botonIzquierda );
